58aad984d124ac49e3280e6a647e1261ec2bd829
Mateusz Nawrocki <mateusz.nawroo@gmail.com>
2023-03-11 11:34:30 +0100

    Created new components for login and registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment.development';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, RegistrationComponent, HomepageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
