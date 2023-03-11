3be1d1ee0265c6dd849093c7800e7a044e964cab
Mateusz Nawrocki <mateusz.nawroo@gmail.com>
2023-03-11 11:23:49 +0100

    Added Firebase and prepare connect with app.

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyCTAsrHPiosCeK53n3JAsWSinmualuWRXA',
+    authDomain: 'books-exchanger-c7e00.firebaseapp.com',
+    projectId: 'books-exchanger-c7e00',
+    storageBucket: 'books-exchanger-c7e00.appspot.com',
+    messagingSenderId: '977022501277',
+    appId: '1:977022501277:web:7c07867301b61a24ffaf74',
+  },
+};
