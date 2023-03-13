726f8ae1953f8f5b420705e1c81429b64e50968f
Mateusz Nawrocki <mateusz.nawroo@gmail.com>
2023-03-13 23:07:42 +0100

    Added button for logout.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent {
+  constructor(private auth: AuthService) {}
 
+  logout() {
+    this.auth.logout();
+  }
 }
